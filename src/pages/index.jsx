b5951c3b08741754d1f02e11b880ce9cc10335e1
Nadeesha Sumanasekara <118651477+NadeeshaSumanasekara@users.noreply.github.com>
2023-09-13 07:24:21 +0530

    Updated

---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+export { default as Overview } from './Overview'; 
+export { default as Wards } from './Wards';  
+export { default as Consultants } from './Consultants';
+export { default as Doctors } from './Doctors'; 
+export { default as Calendar } from './Calendar';
+ 
+
